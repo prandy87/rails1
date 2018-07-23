8a95dbcd10c52ecc57e969d1bdaccbc196de43d9
Pascal Mercat <pascal.mercat@gmail.com>
2018-07-23 17:25:21 +0200

    First commit

---
+++ the_gossip_project/README.md
@@ -0,0 +1,5 @@
+First steps for THP using the sinatra gem for fast hosting. 
+Having fun with learning GET and POST. 
+Work in progress...
+
+ 
---
+++ the_gossip_project/lib/app.rb
@@ -0,0 +1,22 @@
+
+require 'gossip'                                         #tells our app to use gossip.rb
+
+class GossipProjectApp < Sinatra::Base
+	get '/' do
+		erb :index, locals: {gossips: Gossip.all}             #erb -> retrive index.reb, locals -> get local variables, here those in class Gossip, with self method. See Gossip.rb. 
+		 
+	end
+	get '/gossips/new' do
+		erb  :new_gossip
+	end 
+	post '/gossips/new' do 
+		Gossip.new("#{params["gossip_author"]}", "#{params["gossip_content"]}").save          #applies new and save
+	
+	
+		puts "#{params}"
+		puts "#{params["gossip_author"]}"
+		puts "#{params["gossip_content"]}"             #will print to console, this is optional, but avoids ugly code being default printed on page.
+		redirect '/'                                   #will redirect after to 'home'
+		
+	end
+end
